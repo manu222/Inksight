1cd61f4f46216e0dce8e80e6edbe405b9e3f90c2
JoseVicenteB <127752815+JoseVicenteB@users.noreply.github.com>
2023-03-20 18:44:07 +0100

    jose prueba

--- src/main/java/app/Inksight/PruebaClase.java
+++ src/main/java/app/Inksight/PruebaClase.java
@@ -4,9 +4,9 @@ import java.util.*;
 
 public class PruebaClase {//usuario con limite de 100 amigos
 	
-	static Scanner teclado = new Scanner(System.in);
+
 	String nombre;
-	Vector<PruebaClase> listaAmigos;
+	HashSet<PruebaClase> listaAmigos; //probar hashmap
 	String[] libroLeer;
 	String[] libroLeido;
 	int permiso;
@@ -14,26 +14,9 @@ public class PruebaClase {//usuario con limite de 100 amigos
 	int puntos;
 	
 	public void anyadirAmigo(Scanner teclado, PruebaClase usuario){
-		listaAmigos.addElement(usuario);//hacer esta wea un hashset
-		System.out.println(listaAmigos.capacity());
-		
-		
-	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		PruebaClase Fea = new PruebaClase();
-		PruebaClase Jose = new PruebaClase();
-		Fea.nombre = "Paula";
-		Jose.nombre = "Jose";
-		Fea.listaAmigos = new Vector<PruebaClase>(1,1);
-		Fea.anyadirAmigo(teclado, Jose);
-		Fea.anyadirAmigo(teclado, Jose);
-		
-		
-	}
-	
-	
+		listaAmigos.add(usuario);//hacer esta wea un hashset
 
+		
+		
+	}
 }
