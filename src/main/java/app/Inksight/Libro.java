11466044017ea18d984904b359c735d0d362774a
Manu <manuaraba@gmail.com>
2023-05-03 13:53:56 +0200

    books y DB modificado

--- src/main/java/app/Inksight/Libro.java
+++ src/main/java/app/Inksight/Libro.java
@@ -2,21 +2,27 @@ package app.Inksight;
 
 public class Libro {
 
+
+
+
     public int bookID;
     public String title;
     public String authors;
     public int numPages;
     public String publication_date;
     public String languageCode;
-    public String publisher;
-    public Libro(int bookID, String title, String authors, int numPages, String publication_date, String languageCode,String publisher) {
+
+    public Libro(int bookID, String title, String authors, int numPages, String publication_date, String languageCode) {
         this.bookID = bookID;
         this.title = title;
         this.authors = authors;
         this.numPages = numPages;
         this.publication_date = publication_date;
         this.languageCode = languageCode;
-        this.publisher = publisher;
+    }
+
+    public void setID(int bookID) {
+        this.bookID= bookID;
     }
 
     public void setTitle(String title) {
@@ -59,11 +65,25 @@ public class Libro {
 
     public String getLanguageCode() {return languageCode;}
 
-    public String getPublisher() {
-        return publisher;
+    @Override
+    public String toString() {
+        return  "\nbookID=" + bookID +
+                "title=" + title + "\n" +
+                "authors=" + authors + "\n" +
+                "numPages=" + numPages + "\n" +
+                "publication_date=" + publication_date + "\n" +
+                "languageCode=" + languageCode + "\n";
+    }
+    @Override
+    public boolean equals(Object other){
+        if (other==this){
+            return true;
+        }
+        if (other instanceof Libro){
+            Libro l=(Libro) other;
+            return l.getbookID()==this.getbookID();
+        }
+        return  false;
     }
 
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
 }
